e93fcc0dfa0cb45f7d4e3f17e3c36ebe61cd09f1
Nathan Walker <walkerrunpdx@gmail.com>
2022-10-16 10:02:51 -0700

    feat: add example generator

---
+++ tools/generators/add/index.ts
@@ -0,0 +1,13 @@
+import { generateFiles, joinPathFragments, Tree } from '@nrwl/devkit';
+import { library as libraryGenerator } from '@nativescript/nx/src/generators/library/library';
+
+export default async function(tree: Tree, schema: any) {
+  const name = schema.name;
+  await libraryGenerator(tree, {name});
+  tree.delete(`libs/nativescript-${name}/src`);
+  generateFiles(tree, joinPathFragments(__dirname, 'files', 'libs'), `./packages/nativescript-${name}`, {
+    name,
+    tmpl: '',
+    dot: '.',
+  });
+}
