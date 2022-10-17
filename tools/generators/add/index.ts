8802e2d99e016a8c4b590dd315b2f1ad90165d7e
Nathan Walker <walkerrunpdx@gmail.com>
2022-10-16 18:30:41 -0700

    chore: add lib example generator

--- tools/generators/add/index.ts
+++ tools/generators/add/index.ts
@@ -1,13 +1,27 @@
 import { generateFiles, joinPathFragments, Tree } from '@nrwl/devkit';
 import { library as libraryGenerator } from '@nativescript/nx/src/generators/library/library';
 
-export default async function(tree: Tree, schema: any) {
+export default async function (tree: Tree, schema: any) {
   const name = schema.name;
-  await libraryGenerator(tree, {name});
-  tree.delete(`libs/nativescript-${name}/src`);
-  generateFiles(tree, joinPathFragments(__dirname, 'files', 'libs'), `./packages/nativescript-${name}/src`, {
-    name,
-    tmpl: '',
-    dot: '.',
-  });
+  await libraryGenerator(tree, { name });
+  tree.delete(`libs/nativescript-${name}/src/lib/nativescript-${name}.spec.ts`);
+  tree.delete(`libs/nativescript-${name}/src/lib/nativescript-${name}.ts`);
+  tree.write(
+    `libs/nativescript-${name}/src/index.ts`,
+    `/**
+  * This file can be ignored when creating projects.
+  * Only used for demoing apps within this workspace.
+  * Should contain empty implementations
+  */`
+  );
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, 'files', 'libs'),
+    `./packages/nativescript-${name}/src`,
+    {
+      name,
+      tmpl: '',
+      dot: '.',
+    }
+  );
 }
